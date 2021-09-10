2fdd28b9e927cb59226675e05fe0455d37eb9d1f
Olaoluwa1995 <olakulehinstephen@gmail.com>
2021-09-10 07:41:35 +0100

    Clean up

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:popular_tv_show/config/size-config.dart';
+import 'package:popular_tv_show/views/landing-screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -23,75 +24,5 @@ class MyApp extends StatelessWidget {
         );
       });
     });
-  }
-}
-
-class LandingScreen extends StatefulWidget {
-  LandingScreen({Key? key}) : super(key: key);
-
-  @override
-  _LandingScreenState createState() => _LandingScreenState();
-}
-
-class _LandingScreenState extends State<LandingScreen> {
-
-  @override
-  Widget build(BuildContext context) {
-    final size = MediaQuery.of(context).size;
-    return Scaffold(
-      body: Container(
-        height: size.height,
-        width: size.width,
-        decoration: BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage('assets/bg.jpg'),
-            fit: BoxFit.cover
-          ),
-        ),
-        child: Container(
-          width: size.width,
-          color: Colors.black.withOpacity(0.6),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                'Watch the most popular and super amazing TV shows', 
-                style: TextStyle(
-                  fontSize: SizeConfig.textMultiplier * 3,
-                  color: Colors.white,
-                  fontWeight: FontWeight.w600
-                ),
-                textAlign: TextAlign.center,
-              ),
-              SizedBox(
-                height: SizeConfig.heightMultiplier * 6,
-              ),
-              InkWell(
-                onTap: () {},
-                child: Container(
-                  height: SizeConfig.heightMultiplier * 6,
-                  width: SizeConfig.widthMultiplier * 40,
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  child: Center(
-                    child: Text(
-                      'Get Started',
-                      style: TextStyle(
-                        fontSize: SizeConfig.textMultiplier * 2.4,
-                        fontWeight: FontWeight.w800,
-                        color: Colors.black87,
-                      ),
-                    ),
-                  )
-                ),
-              )
-            ],
-          )
-        )
-
-      )
-    );
   }
 }
